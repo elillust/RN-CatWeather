239bdc1cbcb2df98b5a66dd4d89173db52bd8936
까망베르냥 <cute.lovely.cat@kakao.com>
2022-03-22 00:29:54 +0900

    study 2.4

--- App.js
+++ App.js
@@ -1,24 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Test 한글이 안되낭</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View></View>
+      <View></View>
+      <View></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ef4056',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 28,
-    color: '#000',
-  },
-});
